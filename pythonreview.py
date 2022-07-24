9f4a45445254a36b6bf0727390c34d33de3a00ae
yuvalgd23-meet <109908289+yuvalgd23-meet@users.noreply.github.com>
2022-07-24 18:36:16 +0300

    Update pythonreview.py
    created python review

--- pythonreview.py
+++ pythonreview.py
@@ -2,7 +2,7 @@ def create_youtube_video(title, discription):
 	
 	title = input("enter title")
 	discription = input("enter discription")
-	dict1 = {"title": title, "discription": discription,"likes":0, "dislikes":0, "comments:0" }
+	dict1 = {"title": title, "discription": discription,"likes":0, "dislikes":0, "comments":{} }
 	input("enter comment")
 	input
 	dict2 = {"username:}
